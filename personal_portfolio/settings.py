5c26df3c5a7ee1786244d9c6885a8921f5a3d0af
jaypee15 <okoyejohnpaul15@gmail.com>
2023-01-16 17:56:21 +0100

    'updated_again'

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ## For example, for a site URL at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-7190.up.railway.app', '127.0.0.1','www.web-production-7190.up.railway.app','https://web-production-7190.up.railway.app']
 
 # During development, you can instead set just the base URL
 # (you might decide to change the site a few times).
